080e5c11a8db7832f34e6ec73ae5b8850c53ef89
Victor <rusuvictor30@gmail.com>
2021-10-14 23:13:24 -0400

    Init

---
+++ checkout-field.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WC Checkout File Filed Plugin
+ * Description: This plugin will add a Custom WooCommerce File Field on the Checkout Page
+ * Plugin URI: https://vicodemedia.com
+ * Author: Victor Rusu
+ * Version: 1
+**/
+
+//* Don't access this file directly
+defined( 'ABSPATH' ) or die();
