f21cd71d917784bad45dc6d95389d49f84aef579
Victor Rusu <victor@Vicode-Media.lan>
2022-08-29 02:08:40 -0400

    Correction

--- checkout-field.php
+++ checkout-field.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: WC Checkout File Filed Plugin
- * Description: This plugin will add a Custom WooCommerce File Field on the Checkout Page
+ * Plugin Name: WC Checkout File Type Field
+ * Description: This plugin will add a Custom WooCommerce File Type Field on the Checkout Page
  * Plugin URI: https://vicodemedia.com
  * Author: Victor Rusu
  * Version: 1
