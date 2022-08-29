5c19c0990a9e6d0c8bc79083f73277385df6b165
Victor Rusu <victor@Vicode-Media.lan>
2022-08-29 02:04:17 -0400

    Activation hooks

--- checkout-field.php
+++ checkout-field.php
@@ -9,3 +9,32 @@
 
 //* Don't access this file directly
 defined( 'ABSPATH' ) or die();
+
+/**
+ * Register the "book" custom post type
+ */
+function vicode_setup_field() {
+    // register_post_type( 'book', ['public' => true ] );
+    // return "Testing";
+} 
+add_action( 'init', 'vicode_setup_field' );
+ 
+ 
+/**
+ * Activate the plugin.
+ */
+function vicode_activate() { 
+    // Trigger our function that registers the custom post type plugin.
+    vicode_setup_field(); 
+}
+register_activation_hook( __FILE__, 'vicode_activate' );
+
+
+/**
+ * Deactivation hook.
+ */
+function vicode_deactivate() {
+    // Unregister the post type, so the rules are no longer in memory.
+    // unregister_post_type( 'book' );
+}
+register_deactivation_hook( __FILE__, 'vicode_deactivate' );
