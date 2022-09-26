b16acf27a1f80f608dbbcb7b74544522329172fd
Victor Rusu <victor@Vicode-Media.lan>
2022-09-26 18:47:11 -0400

    Text UI added

--- checkout-field.php
+++ checkout-field.php
@@ -13,29 +13,51 @@ defined( 'ABSPATH' ) or die();
 /**
  * Register the "book" custom post type
  */
-function vicode_setup_field() {
-    // register_post_type( 'book', ['public' => true ] );
-    // return "Testing";
-} 
-add_action( 'init', 'vicode_setup_field' );
+// function vicode_setup_field() {
+//     // register_post_type( 'book', ['public' => true ] );
+//     // return "Testing";
+// } 
+// add_action( 'init', 'vicode_setup_field' );
  
-// small changes
+// // small changes
+
+// /**
+//  * Activate the plugin.
+//  */
+// function vicode_activate() { 
+//     // Trigger our function that registers the custom post type plugin.
+//     vicode_setup_field(); 
+// }
+// register_activation_hook( __FILE__, 'vicode_activate' );
+
+
+// /**
+//  * Deactivation hook.
+//  */
+// function vicode_deactivate() {
+//     // Unregister the post type, so the rules are no longer in memory.
+//     // unregister_post_type( 'book' );
+// }
+// register_deactivation_hook( __FILE__, 'vicode_deactivate' );
 
 /**
- * Activate the plugin.
- */
-function vicode_activate() { 
-    // Trigger our function that registers the custom post type plugin.
-    vicode_setup_field(); 
-}
-register_activation_hook( __FILE__, 'vicode_activate' );
 
+* Add custom field to the checkout page
 
-/**
- * Deactivation hook.
- */
-function vicode_deactivate() {
-    // Unregister the post type, so the rules are no longer in memory.
-    // unregister_post_type( 'book' );
+*/
+
+add_action('woocommerce_after_order_notes', 'custom_checkout_field');
+
+function custom_checkout_field($checkout)
+{
+    echo '<div id="custom_checkout_field"><h2>' . __('New Heading') . '</h2>';
+    
+    woocommerce_form_field('custom_field_name', array(
+        'type'          => 'text',
+        'class'         => array('my-field-class form-row-wide'),
+        'label'         => __('Custom Additional Field') ,
+        'placeholder'   => __('New Custom Field') ,
+    ),
+    $checkout->get_value('custom_field_name'));
+    echo '</div>';
 }
-register_deactivation_hook( __FILE__, 'vicode_deactivate' );
